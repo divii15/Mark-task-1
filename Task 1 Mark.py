76cb33e38f13acb0568398760c4c1196b68f7041
divii15 <divyadaksha143@gmail.com>
2024-12-28 15:17:20 +0530

    My next task(1)(9) 28/12

---
+++ Task 1 Mark.py
@@ -0,0 +1,9 @@
+s1=int(input("Enter mark for tamil:"))
+s2=int(input("Enter mark for english:"))
+s3=int(input("Enter mark for maths:"))
+s4=int(input("Enter mark for social science:"))
+s5=int(input("Enter mark for science:"))
+Total=s1+s2+s3+s4+s5
+print("Total mark =",Total)
+avg=Total/5
+print("Average =",avg)
